f324fc7342f81066f4e6bb7025b0d5b7e3131ce2
scottdurow <scott_durow@hotmail.com>
2023-06-14 14:48:35 -0700

    added Record type for parameters to allow treating as name/values

--- pcf-field-portal-tester/ContextExtended.ts
+++ pcf-field-portal-tester/ContextExtended.ts
@@ -1,6 +1,6 @@
 /*specifying events types here until it is unlocked by platform*/
 export interface ContextExtended<T> extends ComponentFramework.Context<T> {
-    parameters: T & Record<string, any>;
+    parameters: T & Record<string, ComponentFramework.PropertyTypes.Property>;
     events: IEventBag;
 }
 export declare type IEventBag = Record<string, () => void>;
